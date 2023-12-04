ef6614cd1e5eb5221475ad45dfa4ed1e91f5b850
im222b <rudgnsdleh@naver.com>
2023-12-04 17:27:06 +0900

    day01-3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-`; 
+`;
 
-const Input = styled.input.attrs({required:true, minLength:10})`
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: teal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation:${animation} 2s linear infinite;
+  span {
+    color: white;
+    font-size: 35px;
+    &:hover{
+      font-size:  60px;
+    }
+    &:active{
+      opacity:  0;
+    }
+  }
 `;
 
 function App() {
   return( 
-  <Father as="header"> 
-    <Input />
-    <Input />
-    <Input />
-    <Input />
-    <Input />
-  </Father>
+    <Wrapper>
+      <Box >
+        <span>B</span>
+      </Box>
+    </Wrapper>
 ); 
 }
 
