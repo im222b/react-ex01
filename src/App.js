adbcbe4e244d1e19eec1812a6b73d39b1c97c808
im222b <rudgnsdleh@naver.com>
2023-12-04 18:11:00 +0900

    day01-5

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import styled, {keyframes} from "styled-components";
 
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+`
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    
-    border-radius: 100px;
-  }
-  100%{
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
 
-const Text = styled.span`
-  
-  font-size: 35px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: teal;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation:${animation} 2s linear infinite;
-  ${Text} {
-    color: white;
-    &:hover{
-      font-size:  60px;
-      
-    }
-  }
-`;
 
 function App() {
   return( 
     <Wrapper>
-      <Box >
-        <Text as="p">B</Text>
-      </Box>
-      <Text>A</Text>
+      <Title>HEllO</Title>
     </Wrapper>
 ); 
 }
