0b7aa124146ed41f273400187d97852cea346cd9
im222b <rudgnsdleh@naver.com>
2023-12-04 16:31:07 +0900

    day01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColer};
+  height: 100px;
+  width: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius : 50px;
+`;
+
+const Text = styled.h2`
+  color: white;
+  text-align: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return( 
+  <Father>
+    <Box bgColer="teal">
+      <Text>Hello</Text>
+    </Box>
+    <Circle bgColer="tomato"/>
+  </Father>
+); 
 }
 
 export default App;
