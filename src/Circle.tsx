9e1ca515340fd2b65020378d7d6b78d8c1663731
im222b <rudgnsdleh@naver.com>
2023-12-06 16:39:01 +0900

    day02

---
+++ src/Circle.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+interface CircleProps {
+    bgColor:string;
+}
+
+const Container = styled.div<CircleProps>`
+    width: 200px;
+    height: 200px;
+    background-color: ${props => props.bgColor};
+    border-radius: 100px;
+    `;
+
+
+
+function Circle({bgColor}:CircleProps){
+    return(
+        <Container bgColor={bgColor} />
+    );
+}
+
+export default Circle;
+
+interface PlayerShape {
+    name: string
+    age: number
+}
+
+
+const sayHello = (playerObj:PlayerShape) =>
+        `Hello ${playerObj.name} you are ${playerObj.age}years old.`
+
+
+sayHello({name:"2B", age:30})
