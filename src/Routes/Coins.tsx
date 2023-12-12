e6ce2e9709f9840aa736695a9d22580e9511c4ba
im222b <rudgnsdleh@naver.com>
2023-12-12 17:41:25 +0900

    day03-1

--- src/Routes/Coins.tsx
+++ src/Routes/Coins.tsx
@@ -1,10 +1,93 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width:  480px;
+    margin:  0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0 50px 0;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+    background-color: white;
+    color: ${(props) => props.theme.bgColor};
+    border-radius: 20px;
+    margin-bottom: 10px;
+    a{
+        padding: 20px;
+        transition: color 0.2s ease-in;
+        display: block;
+    }
+    &:hover {
+        a{
+            color:${(props)=> props.theme.accentColor}
+        }
+    }
+`;
+
+const Loader =styled.span`
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    color:${(props)=> props.theme.textColor};
+    font-size: 38px;
+
+`;
+
 const Title = styled.h1`
+    font-size: 48px;
     color: ${(props) => props.theme.accentColor};
 `
 
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
+
 function Coins() {
-    return <Title>COINS</Title>;
+    const [coins, setCoins] = useState<CoinInterface[]> ([]);
+    const [loding, setLodoing] = useState(true);
+    useEffect(() => {
+        (async() => {
+        const response =  await fetch("https://api.coinpaprika.com/v1/coins");
+        const json = await response.json();
+        setCoins(json.slice(0,100));
+        setLodoing(false);
+        })();
+    }, []);
+    return (
+    <Container>
+        <Header>
+            <Title>COINS</Title>
+        </Header>
+        {loding ? (
+            <Loader>Loding...</Loader>
+        ): (
+            <CoinsList>
+            {coins.map((coin) => (
+            <Coin key = {coin.id}>
+                <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                </Coin>
+                ))} 
+        </CoinsList>
+        )}
+    </Container>
+    );
 }
 export default Coins;
